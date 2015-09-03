b9d683f6264e0fda50d1c744ea7f5dc52db3fdb8
Troi-Ryan R. S <troiryan2@gmail.com>
2015-09-02 22:27:34 -0700

    engine

--- src/Engine.java
+++ src/Engine.java
@@ -1,5 +1,5 @@
 /*
- * Author(s): Ryan Chen
+ * Author(s): Ryan Chen and Troi-Ryan S.
  * Date: Sepetember 02, 2015
  * Description: The V6 Engine of our program. VROOM VROOM!
  */
@@ -27,24 +27,24 @@ public class Engine {
 	public static void main(String[] args) {
 		//This list is a list of the objects that use Drawable.
 		drawableList = new ArrayList<Drawable>();
-		frame = new JFrame("P1-StoefflerChen"); //jframe
+		frame = new JFrame("P1-Stoeffler-Chen"); //jframe
 		frame.setPreferredSize(new Dimension(800, 600));
 		frame.setMinimumSize(new Dimension(300, 300)); 
-		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); //crash java.exe
 		frame.setContentPane(new JPanel(){
 			@Override
 				//This paints the objects onto the content panel.
 			   public void paintComponent(Graphics g) {
-				try {
-					snoo.setX(this.getWidth()/2 - 110);
+				try { //somehow concurrent modification exceptions are thrown, possible due to the changes via the timer; so we are just going to skip over those times when it throws it
+					snoo.setX(this.getWidth()/2 - 110); //center the beautiful snoo
 					snoo.setY(this.getHeight()/2 - 150);
 				    Graphics2D render = (Graphics2D) g;
-				    render.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
-				    for(Drawable object : drawableList) if(object != null) object.paint(render);
+				    render.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON); //turn AA on to avoid hard edges
+				    for(Drawable object : drawableList) if(object != null) object.paint(render); //paint all the drawable objects
 				} catch(Exception e) { }
 			}
 		});
-		frame.pack();
+		frame.pack(); //set those sizes
 		frame.setVisible(true);
 		//This creates ALOT of upvote arrows.
 		for(int y = 0; y <= frame.getHeight() + 5000; y += 50){
@@ -53,7 +53,7 @@ public class Engine {
 			}
 		}
 		//Here, the snoo is initialized.
-		snoo = new Snoo(0, 0, 220, 300);
+		snoo = new Snoo(0, 0, 220, 300); //snoo <3 -t
 		add(snoo);
 		//Here I create the CatHead
 		CatHead c = new CatHead(200, 100,3,3);
@@ -62,7 +62,7 @@ public class Engine {
 		Timer timer = new Timer(33, new ActionListener(){
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
-				snoo.rotate(5);
+				snoo.rotate(5); //snoo is a beautiful being -t
 				frame.repaint();
 			}
 		});
