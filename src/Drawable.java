942f0c67ad1308101ec3c70dde9e70468f6ddc19
Troi-Ryan R. S <troiryan2@gmail.com>
2015-09-02 22:11:10 -0700

    drawable comment code

--- src/Drawable.java
+++ src/Drawable.java
@@ -1,8 +1,16 @@
+/*
+ * Troi-Ryan Stoeffler's awesome drawable type
+ * It's beautiful isn't it?
+ * 
+ * The drawable interface, for types of objects that want to be drawn onto the custom frame
+ * easily--maybe;
+ */
+
 import java.awt.Graphics2D;
 
-
 public interface Drawable {
-	public void paint(Graphics2D render);
+	
+	public void paint(Graphics2D render); //create abstract methods to be inherited into the classes that implement tihs class
 	public void setX(int x);
 	public void setY(int y);
 	public int getX();
